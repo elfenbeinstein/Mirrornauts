b35adfcd54702d22276ad2c927acda5f9956d983
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2022-09-12 20:46:59 +0200

    small spawn update

---
+++ Mirror Matrix/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spawn
+{
+    public string name;
+    public GameObject unit;
+    public int round;
+    public int countdown;
+    public int liftoff;
+}
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
