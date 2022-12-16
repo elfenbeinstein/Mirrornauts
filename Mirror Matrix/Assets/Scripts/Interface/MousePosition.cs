4644383a0d383511dec5bdb378870728c5a94c7f
Leon <leon@nyoucomer.de>
2022-12-16 16:48:59 +0100

    Added Mouse Coordinates on press Ctrl

---
+++ Mirror Matrix/Assets/Scripts/Interface/MousePosition.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MousePosition : MonoBehaviour
+{
+    /// <summary>
+    /// Gets the mouse position and translates it into coordinates when holding Ctrl
+    /// It gets displayed next to mouse
+    /// </summary>
+    /// 
+    [SerializeField] Vector2 maxCoordinats;
+    [SerializeField] Vector2 textBoxOffset;
+    [SerializeField] GameObject mouseCoord;
+    Vector3 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get mouse world position
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Check that mouse Pos is in coordinate system
+        if (mousePos.x <= maxCoordinats.x && mousePos.y <= maxCoordinats.y)
+        {
+            if(mousePos.x >= -maxCoordinats.x && mousePos.y >= -maxCoordinats.y)
+            {
+                if (Input.GetKey(KeyCode.LeftControl))
+                {
+                    mouseCoord.GetComponentInChildren<TextMeshProUGUI>().text = mousePos.x.ToString("F1") + " / " + mousePos.y.ToString("F1");
+                    mouseCoord.gameObject.SetActive(true);
+                    mouseCoord.gameObject.transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, new Vector2 (mousePos.x + textBoxOffset.x, mousePos.y + textBoxOffset.y));
+                }
+                else
+                {
+                    mouseCoord.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                mouseCoord.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            mouseCoord.gameObject.SetActive(false);
+        }
+    }
+}
